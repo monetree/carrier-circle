d924bb6481232296a80be462ea6811c3fc33b18a
monetree <38796292+monetree@users.noreply.github.com>
2018-09-28 18:09:39 +0530

    adding changes

--- backend/details/models.py
+++ backend/details/models.py
@@ -8,29 +8,31 @@ from scrap import models as scrap_models
 """
 
 class UpscJobDetails(models.Model):
-    upsc_id    = models.AutoField(primary_key=True)
-    start_date = models.CharField(max_length=60)
-    last_date  = models.CharField(max_length=60)
-    post_name  = models.CharField(max_length=255)
-    education  = models.CharField(max_length=255)
-    more_info  = models.TextField()
-    type       = models.IntegerField()
-    job_id     = models.IntegerField(default=None,blank=True,null=True)
-    join_id    = models.IntegerField(default=None,blank=True,null=True)
+    upsc_details_id = models.AutoField(primary_key=True)
+    upsc            = models.OneToOneField(scrap_models.UpscJobs,on_delete=models.CASCADE,related_name='upsc_details')
+    start_date      = models.CharField(max_length=60)
+    last_date       = models.CharField(max_length=60)
+    post_name       = models.CharField(max_length=255)
+    education       = models.CharField(max_length=255)
+    more_info       = models.TextField()
+    type            = models.IntegerField()
+    job_id          = models.IntegerField(default=None,blank=True,null=True)
+    join_id         = models.IntegerField(default=None,blank=True,null=True)
 
     def __str__(self):
         return "Upsc JobDetails"
 
 class SscJobDetails(models.Model):
-    ssc_id     = models.AutoField(primary_key=True)
-    start_date = models.CharField(max_length=60)
-    last_date  = models.CharField(max_length=60)
-    post_name  = models.CharField(max_length=255)
-    education  = models.CharField(max_length=255)
-    more_info  = models.TextField()
-    type       = models.IntegerField()
-    job_id     = models.IntegerField(default=None,blank=True,null=True)
-    join_id    = models.IntegerField(default=None,blank=True,null=True)
+    ssc_details_id = models.AutoField(primary_key=True)
+    ssc            = models.OneToOneField(scrap_models.SscJobs,on_delete=models.CASCADE,related_name='ssc_details')
+    start_date     = models.CharField(max_length=60)
+    last_date      = models.CharField(max_length=60)
+    post_name      = models.CharField(max_length=255)
+    education      = models.CharField(max_length=255)
+    more_info      = models.TextField()
+    type           = models.IntegerField()
+    job_id         = models.IntegerField(default=None,blank=True,null=True)
+    join_id        = models.IntegerField(default=None,blank=True,null=True)
 
     def __str__(self):
         return "Ssc JobDetails"
